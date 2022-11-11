929289dd5d29f23ec1bdbe2470adb6dcdc0604af
Alina Vakulenko <114286140+alina-vakulenko@users.noreply.github.com>
2022-11-11 17:26:15 +0200

    modified links in UnitsChoice.js

--- src/UnitsChoice.js
+++ src/UnitsChoice.js
@@ -4,12 +4,18 @@ import "./UnitsChoice.css";
 export default function UnitsChoice() {
   return (
     <div className="UnitsChoice">
-      <a href="#" id="metric" title="Switch to metric" className="active">
+      <a
+        href="/"
+        id="metric"
+        title="Switch to metric"
+        className="active"
+        rel="noreferrer"
+      >
         metric{" "}
       </a>
       |{" "}
       <span>
-        <a href="#" id="imperial" title="Switch to imperial">
+        <a href="/" id="imperial" title="Switch to imperial" rel="noreferrer">
           imperial
         </a>
       </span>
